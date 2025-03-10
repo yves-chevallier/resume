c91c6259d54bcc6b424663d5fc03b634242367de
Yves Chevallier <yves.chevallier@heig-vd.ch>
2025-03-10 17:35:35 +0100

    Update VuePress 2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,15 @@
-import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
+  port: 4000,
   title: 'Yves Chevallier',
   description: 'Have a look at my resumé and my projects',
-  navbar: false,
+  theme: defaultTheme({
+    //logo: 'https://vuejs.press/images/hero.png',
+    navbar: false, //['/', '/get-started'],
+  }),
+  bundler: viteBundler(),
 })
